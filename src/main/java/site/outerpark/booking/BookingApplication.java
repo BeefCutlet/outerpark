7cde8bb6d69d156018c3f72b8d9ba0a2440be024
OSU <whysabi492@naver.com>
2024-03-28 12:01:44 +0900

    프로젝트 시작

---
+++ src/main/java/site/outerpark/booking/BookingApplication.java
@@ -0,0 +1,13 @@
+package site.outerpark.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingApplication.class, args);
+	}
+
+}
