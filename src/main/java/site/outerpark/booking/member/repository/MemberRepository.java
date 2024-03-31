5e14f1cc42a91ee867795643e299297f3d35b7ad
OSU <whysabi492@naver.com>
2024-04-01 01:22:31 +0900

    feat: 회원 아이디로 회원 정보 조회 메서드 추가

--- src/main/java/site/outerpark/booking/member/repository/MemberRepository.java
+++ src/main/java/site/outerpark/booking/member/repository/MemberRepository.java
@@ -3,5 +3,8 @@ package site.outerpark.booking.member.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import site.outerpark.booking.member.domain.entity.Member;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long> {
+    Optional<Member> findMemberByUid(String uid);
 }
