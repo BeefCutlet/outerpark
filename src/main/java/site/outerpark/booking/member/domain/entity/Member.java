a7bba4441b4c487e4647606afde6e395434f002d
OSU <whysabi492@naver.com>
2024-04-01 01:23:53 +0900

    feat: 회원 비밀번호 수정 및 검증 메서드 추가, 회원 엔티티 생성 메서드 추가, 회원 상태 관련 필드 추가

--- src/main/java/site/outerpark/booking/member/domain/entity/Member.java
+++ src/main/java/site/outerpark/booking/member/domain/entity/Member.java
@@ -1,11 +1,13 @@
 package site.outerpark.booking.member.domain.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import site.outerpark.booking.common.entity.BaseTimeEntity;
+import site.outerpark.booking.member.domain.MemberRole;
+import site.outerpark.booking.member.domain.MemberStatus;
+import site.outerpark.booking.member.dto.request.MemberSaveRequest;
+import site.outerpark.booking.member.dto.request.MemberUpdateRequest;
 
 @Entity
 @Builder
@@ -29,6 +31,37 @@ public class Member extends BaseTimeEntity {
     private String name;
 
     @Column
-    private String role;
+    @Enumerated(EnumType.STRING)
+    private MemberRole role;
+
+    @Column
+    @Enumerated(EnumType.STRING)
+    private MemberStatus status;
+
+    //회원 정보 저장용
+    public static Member from(MemberSaveRequest memberSaveRequest) {
+        return Member.builder()
+                .uid(memberSaveRequest.getUid())
+                .password(memberSaveRequest.getPassword())
+                .name(memberSaveRequest.getName())
+                .role(MemberRole.USER)
+                .status(MemberStatus.ACTIVE)
+                .build();
+    }
+
+    //회원 정보 수정용
+    public void modifyPassword(String rawPassword, PasswordEncoder passwordEncoder) {
+        this.password = passwordEncoder.encode(rawPassword);
+    }
+
+    //패스워드 일치 여부 확인
+    public boolean isPasswordMatched(String requestPassword, PasswordEncoder passwordEncoder) {
+        return passwordEncoder.matches(requestPassword, this.password);
+    }
+
+    //회원 정보 변경용
+    public void modifyMemberStatus(MemberStatus status) {
+        this.status = status;
+    }
 
 }
