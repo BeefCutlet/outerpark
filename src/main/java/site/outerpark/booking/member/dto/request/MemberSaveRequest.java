1f097910cb1e6276ef4a10c3c7f04208d0c09413
OSU <whysabi492@naver.com>
2024-04-01 01:21:46 +0900

    chore: 회원 정보 저장 및 수정 DTO 업데이트

--- src/main/java/site/outerpark/booking/member/dto/request/MemberSaveRequest.java
+++ src/main/java/site/outerpark/booking/member/dto/request/MemberSaveRequest.java
@@ -4,8 +4,12 @@ import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
 @Getter
-public class MemberUpdateRequest {
+public class MemberSaveRequest {
 
     @NotBlank
+    private String uid;
+    @NotBlank
     private String password;
+    @NotBlank
+    private String name;
 }
