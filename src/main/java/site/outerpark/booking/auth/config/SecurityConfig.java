15fa543a48cbae5f8471427c04dc8d239b54edef
OSU <whysabi492@naver.com>
2024-04-01 01:24:07 +0900

    feat: 비밀번호 인코더 스프링 빈 추가

--- src/main/java/site/outerpark/booking/auth/config/SecurityConfig.java
+++ src/main/java/site/outerpark/booking/auth/config/SecurityConfig.java
@@ -1,11 +1,18 @@
 package site.outerpark.booking.auth.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig {
 
-
+    //비밀번호 인코딩 클래스
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 }
