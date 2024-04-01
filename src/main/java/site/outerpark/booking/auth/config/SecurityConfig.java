04082afcb592a6e67fec6c4d5d65267c2747a07a
OSU <whysabi492@naver.com>
2024-04-02 00:37:30 +0900

    feat: Spring Security 설정 추가

--- src/main/java/site/outerpark/booking/auth/config/SecurityConfig.java
+++ src/main/java/site/outerpark/booking/auth/config/SecurityConfig.java
@@ -2,13 +2,53 @@ package site.outerpark.booking.auth.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import site.outerpark.booking.auth.security.CustomAuthenticationProvider;
+import site.outerpark.booking.auth.security.CustomUserService;
+import site.outerpark.booking.member.repository.MemberRepository;
 
-@EnableWebSecurity
 @Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .formLogin(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((authorize) -> {
+                    authorize.requestMatchers("/login").permitAll()
+                            .anyRequest().authenticated();
+                });
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationProvider authenticationProvider) {
+        return new ProviderManager(authenticationProvider);
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(CustomUserService userDetailsService,
+                                                         PasswordEncoder passwordEncoder) {
+        return new CustomAuthenticationProvider(userDetailsService, passwordEncoder);
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(MemberRepository memberRepository) {
+        return new CustomUserService(memberRepository);
+    }
 
     //비밀번호 인코딩 클래스
     @Bean
