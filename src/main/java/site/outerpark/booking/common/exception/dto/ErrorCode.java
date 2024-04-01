4d2358a1a895007c3ae329a5fc02ff0ad06510c6
OSU <whysabi492@naver.com>
2024-04-02 00:36:50 +0900

    feat: 에러 코드 추가

--- src/main/java/site/outerpark/booking/common/exception/dto/ErrorCode.java
+++ src/main/java/site/outerpark/booking/common/exception/dto/ErrorCode.java
@@ -6,6 +6,7 @@ public enum ErrorCode {
     REQUEST_ARGS_NOT_VALID(HttpStatus.BAD_REQUEST, "입력한 데이터의 형식이 올바르지 않습니다."),
     MEMBER_ALREADY_EXIST(HttpStatus.BAD_REQUEST, "이미 회원이 존재합니다."),
     DATA_NOT_FOUND(HttpStatus.NOT_FOUND, "데이터가 존재하지 않습니다."),
+    ACCESS_DENIED(HttpStatus.FORBIDDEN, "접근이 거부되었습니다."),
     ;
 
     private final HttpStatus status;
