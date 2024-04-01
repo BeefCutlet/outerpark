1975530929f49c4006e0840361fc87a84bca6b9c
OSU <whysabi492@naver.com>
2024-04-02 01:40:04 +0900

    feat: 현재 접속 중인 회원 정보 파라미터 추가

--- src/main/java/site/outerpark/booking/member/controller/MemberController.java
+++ src/main/java/site/outerpark/booking/member/controller/MemberController.java
@@ -1,22 +1,46 @@
 package site.outerpark.booking.member.controller;
 
 import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.*;
+import site.outerpark.booking.auth.dto.LoginMember;
+import site.outerpark.booking.member.dto.request.MemberSaveRequest;
 import site.outerpark.booking.member.dto.request.MemberUpdateRequest;
+import site.outerpark.booking.member.service.MemberService;
 
+@Slf4j
 @RestController
 @RequestMapping("/members")
 @RequiredArgsConstructor
 public class MemberController {
 
+    private final MemberService memberService;
+
+    /**
+     * 회원 정보 저장 (회원가입)
+     */
+    @PostMapping
+    public void saveMember(@RequestBody @Valid final MemberSaveRequest memberSaveRequest) {
+        memberService.saveMember(memberSaveRequest);
+    }
+
     /**
      * 사용자 정보 수정 (마이페이지)
      */
     @PutMapping
-    public void modifyMember(MemberUpdateRequest memberUpdateRequest, HttpSession session) {
+    public void modifyMember(@RequestBody @Valid final MemberUpdateRequest memberUpdateRequest,
+                             @AuthenticationPrincipal LoginMember loginMember) {
+        memberService.modifyMember(loginMember.getUsername(), memberUpdateRequest);
+    }
 
+    /**
+     * 회원 탈퇴 - 회원 상태 변경
+     */
+    @PutMapping("/withdrawal")
+    public void withdrawMember(@AuthenticationPrincipal LoginMember loginMember) {
+        memberService.withdrawMember(loginMember.getUsername());
     }
 }
--- src/main/java/site/outerpark/booking/member/service/MemberService.java
+++ src/main/java/site/outerpark/booking/member/service/MemberService.java
@@ -1,12 +1,64 @@
 package site.outerpark.booking.member.service;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import site.outerpark.booking.common.exception.MemberAlreadyExist;
+import site.outerpark.booking.member.domain.MemberStatus;
+import site.outerpark.booking.member.domain.entity.Member;
+import site.outerpark.booking.member.dto.request.MemberSaveRequest;
+import site.outerpark.booking.member.dto.request.MemberUpdateRequest;
 import site.outerpark.booking.member.repository.MemberRepository;
+
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class MemberService {
 
     private final MemberRepository memberRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    /**
+     * 회원 정보 저장 (회원가입)
+     */
+    public void saveMember(final MemberSaveRequest memberSaveRequest) {
+        //회원 정보가 이미 존재하는지 확인하기 위해 회원 정보 조회
+        final Optional<Member> findMember = memberRepository.findMemberByUid(memberSaveRequest.getUid());
+        if (findMember.isPresent()) {
+            throw new MemberAlreadyExist();
+        }
+
+        //회원 정보 저장
+        final Member member = Member.from(memberSaveRequest);
+        memberRepository.save(member);
+    }
+
+    /**
+     * 회원 정보 수정 (마이페이지)
+     */
+    public void modifyMember(final String uid, final MemberUpdateRequest memberUpdateRequest) {
+        //회원 정보 조회 - 없으면 예외 발생
+        final Member findMember = memberRepository.findMemberByUid(uid).orElseThrow(() -> {
+            throw new IllegalArgumentException("회원 정보가 존재하지 않습니다.");
+        });
+
+        //회원 정보 수정
+        findMember.modifyPassword(memberUpdateRequest.getPassword(), passwordEncoder);
+    }
+
+    /**
+     * 회원 탈퇴
+     */
+    public void withdrawMember(final String uid) {
+        //회원 정보 조회 - 없으면 예외 발생
+        final Member findMember = memberRepository.findMemberByUid(uid).orElseThrow(() -> {
+            throw new IllegalArgumentException("회원 정보가 존재하지 않습니다.");
+        });
+
+        //회원 정보 수정
+        findMember.modifyMemberStatus(MemberStatus.WITHDRAWAL);
+    }
 }
